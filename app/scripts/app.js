c480c6dcdf848bcf45d39523a43adce478b01bdb
Marcelo Morales <marcelomorales.name@gmail.com>
2013-11-18 18:17:34 -0400

    Scaffolding

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('angularCharlaApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
