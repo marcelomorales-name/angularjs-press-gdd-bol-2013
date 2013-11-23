c2aa3d319a634653da058c89d14109fa9ba14b7a
Marcelo Morales <marcelomorales.name@gmail.com>
2013-11-22 20:26:03 -0400

    Preparando la presentacion

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,5 @@
 
 angular.module('angularCharlaApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+        $scope.slide = 0;
   });
